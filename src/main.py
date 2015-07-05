670ac81478d8f32ddfb699c01da4c2458bfbc6f4
Jakob Pupke <jakob.pupke@googlemail.com>
2015-07-05 22:43:11 +0200

    calculation of conditional probabilities

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from os import listdir
 
 if __name__ == "__main__":
     categories_word_count = {}
+    vocabulary = {}
+    cond_prob = {}
     data_path = "../data/"
     train_path = "/train"
     categories = ["sport", "politik", "wirtschaft"]
@@ -12,7 +14,6 @@ if __name__ == "__main__":
             category = parse_data(cat)
             categories_word_count[cat] = create_word_dict(category)
         print_dict(categories_word_count)
-        print(docs_count)
 
     def parse_data(cat):
         full_path = data_path + cat + train_path
@@ -28,6 +29,13 @@ if __name__ == "__main__":
                 for each_word in split_paragraph:
                     text_list.append(each_word)
             cleaned = clean_up_text(text_list)
+
+            vocabulary[cat] = cleaned
+            if "_all_words" in vocabulary:
+                vocabulary["_all_words"] = set(vocabulary["_all_words"]).union(set(cleaned))
+            else:
+                vocabulary["_all_words"] = set(cleaned)
+
             data_set.close()
             return cleaned
 
@@ -58,5 +66,23 @@ if __name__ == "__main__":
             for word in dict[key]:
                 print("   " + word + ": " + str(dict[key][word]))
             print()
+
+    def get_len_of_cat(cat):
+        count = 0
+        category = categories_word_count[cat]
+        for word in category:
+            count += category[word]
+        return count
+
+    def do_condprob():
+        for word in vocabulary["_all_words"]:
+            cond_prob[word] = {}
+            for cat in categories:
+                cond_prob[word][cat] = calc_condprob(word, cat)
+        return cond_prob
+
+    def calc_condprob(word, cat):
+        t_ct = categories_word_count[cat].get(word, 0)
+        return (t_ct + 1) / (get_len_of_cat(cat) + len(vocabulary["_all_words"]))
 
     init()
